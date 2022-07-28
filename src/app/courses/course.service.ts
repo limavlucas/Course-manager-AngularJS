93d2cdee2b0029f6b72f1983ae5bfd34187caf16
Lucas <lucasviniciuslimadepaula@gmail.com>
2022-07-28 15:37:40 -0300

    salvando progresso no projeto do Curso

---
+++ src/app/courses/course.service.ts
@@ -0,0 +1,72 @@
+import { Injectable } from "@angular/core";
+import { Course } from "./course";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CourseService{
+
+    retrieveAll(): Course[] {
+        return COURSES
+    }
+    
+}
+
+
+var COURSES: Course[] = [
+    {
+        id: 1,
+        name: 'Angular: CLI',
+        releasedate: 'November 2, 2019',
+        description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI.',
+        duration: 120,
+        code: 'XLF-1212',
+        rating: 3,
+        price: 12.99,
+        imageurl: '/assets/images/cli.png',
+    },
+    {
+        id: 2,
+        name: 'Angular: Forms',
+        releasedate: 'November 4, 2019',
+        description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de Forms.',
+        duration: 80,
+        code: 'DWQ-3412',
+        rating: 3.5,
+        price: 24.99,
+        imageurl: '/assets/images/forms.png',
+    },
+    {
+        id: 3,
+        name: 'Angular: HTTP',
+        releasedate: 'November 8, 2019',
+        description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de HTTP.',
+        duration: 80,
+        code: 'QPL-0913',
+        rating: 4.0,
+        price: 36.99,
+        imageurl: '/assets/images/http.png',
+    },
+    {
+        id: 4,
+        name: 'Angular: Router',
+        releasedate: 'November 16, 2019',
+        description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de Router.',
+        duration: 80,
+        code: 'OHP-1095',
+        rating: 4.5,
+        price: 46.99,
+        imageurl: '/assets/images/router.png',
+    },
+    {
+        id: 5,
+        name: 'Angular: Animations',
+        releasedate: 'November 25, 2019',
+        description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis sobre Animation.',
+        duration: 80,
+        code: 'PWY-9381',
+        rating: 5,
+        price: 56.99,
+        imageurl: '/assets/images/animations.png',
+    }
+];
